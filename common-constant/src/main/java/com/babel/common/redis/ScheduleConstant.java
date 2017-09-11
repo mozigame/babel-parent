9ce2bf2b378ac4877d2ec10217aa690e41f4d450
justin <justins1707@gmail.com>
2017-09-11 10:53:21 +0800

    change

---
+++ common-constant/src/main/java/com/babel/common/redis/ScheduleConstant.java
@@ -0,0 +1,15 @@
+package com.babel.common.redis;
+
+/**
+ * schedule常量
+ * @author Justin
+ *
+ */
+public class ScheduleConstant {
+	/**
+     * 处理redis数据的间隔时间
+     */
+    public static final int cacheFlushRate = 180000;   //3分钟
+  
+
+}
