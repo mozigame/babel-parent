0f1ee4d9510db89dd1c24cd1b8c4c7e28df4a1c9
justin <justins1707@gmail.com>
2017-10-12 17:03:54 +0800

    status

---
+++ common-constant/src/main/java/com/babel/common/lottery/StatusLock.java
@@ -0,0 +1,42 @@
+package com.babel.common.lottery;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 使用中/冻结
+ * created by roachjiang 2017/9/28
+ */
+public enum StatusLock {
+    ACTIVE(1, "启用中"),
+    LOCK(0, "已冻结");
+
+
+    private static Map<Integer, StatusLock> map = new HashMap<>();
+
+    static {
+        for (StatusLock oj : StatusLock.values()) {
+            map.put(oj.code, oj);
+        }
+    }
+
+    private int code;
+    private String mes;
+
+    StatusLock(int code, String mes) {
+        this.code = code;
+        this.mes = mes;
+    }
+
+    public int code() {
+        return code;
+    }
+
+    public String mes() {
+        return mes;
+    }
+
+    public static StatusLock parse(int code) {
+        return map.get(code);
+    }
+}
