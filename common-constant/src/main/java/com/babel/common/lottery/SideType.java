d3125adee884d2999118eb5790629de1af808bfc
justin <justins1707@gmail.com>
2017-10-17 16:42:12 +0800

    update

--- common-constant/src/main/java/com/babel/common/lottery/SideType.java
+++ common-constant/src/main/java/com/babel/common/lottery/SideType.java
@@ -11,8 +11,11 @@ import java.util.Map;
  */
 public enum SideType {
 
-    official(1, "官方彩"),
-    dual(2, "双面彩");
+    official(1, "时时彩-官方彩"),
+    dual(2, "时时彩-双面彩"),
+    elevenFive_official(3, "11选5-官方彩"),
+    elevenFive_dual(4, "11选5-双面彩");
+	
 
 
     private int code;
