17dfab5f819e248b7082ff3be1a513b2e72c2e19
justin <justins1707@gmail.com>
2017-10-23 12:00:59 +0800

    彩种类型

--- common-constant/src/main/java/com/babel/common/lottery/LotteryType.java
+++ common-constant/src/main/java/com/babel/common/lottery/LotteryType.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 追号类型
+ * 彩种类型
  * created by roachjiang 2017/9/28
  */
 public enum LotteryType {
     SSC(1, "时时彩"),//shi shi cai
     ESF(2, "11选5"),//Eleven select file
-    SIX(3, "六合彩");//six lottery
+    Q3(3, "快3"),//quick 3
+    SIX(6, "六合彩");//six lottery
+	
 
 
     private static Map<Integer, LotteryType> map = new HashMap<>();
