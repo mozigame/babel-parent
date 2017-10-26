791cf9f11d0cf2dbb40a3e36bc267df0b1d79d4c
joey <jianghao2>
2017-10-26 13:58:39 +0800

    订单状态增加和，平局

---
+++ common-constant/src/main/java/com/babel/common/lottery/IsTied.java
@@ -0,0 +1,21 @@
+package com.babel.common.lottery;
+
+/**
+ * User: joey
+ * Date: 2017/10/25
+ * Time: 10:57
+ * 是否是平局
+ */
+public enum IsTied {
+
+    no(0),
+    yes(1);
+
+    private int code;
+
+    IsTied(int code) {
+        this.code = code;
+    }
+
+
+}
