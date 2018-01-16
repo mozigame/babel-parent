ff66fc0312842df9425980b1e352a8ae19e988a1
justin <justins1707@gmail.com>
2018-01-16 13:52:09 +0800

    update

--- common-core/src/main/java/com/babel/common/core/util/RedisZsetUtil.java
+++ common-core/src/main/java/com/babel/common/core/util/RedisZsetUtil.java
@@ -2,23 +2,35 @@ package com.babel.common.core.util;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 
 public class RedisZsetUtil {
+	private static final Logger logger = Logger.getLogger(RedisZsetUtil.class);
 	private static RedisTemplate redisTemplate=null;
 	public static void add(String redisKey, String data, Long time){
-		redisTemplate=RedisUtil.getRedisTemplate();
+		if(redisTemplate==null){
+			redisTemplate=RedisUtil.getRedisTemplate();
+		}
 		if(redisTemplate!=null){
-			redisTemplate.opsForZSet().add(redisKey, data, time);
+			redisTemplate.opsForZSet().add(redisKey, data, time);	
+		}
+		else{
+			logger.warn("----add--redisTemplate is null");
 		}
 	}
 	
 	public static Set<TypedTuple<String>> geTuples(String redisKey, Long time){
-		redisTemplate=RedisUtil.getRedisTemplate();
+		if(redisTemplate==null){
+			redisTemplate=RedisUtil.getRedisTemplate();
+		}
 		if(redisTemplate!=null){
 			Set<TypedTuple<String>> set=RedisUtil.getRedisTemplate().opsForZSet().reverseRangeByScoreWithScores(redisKey, time, -1);
 			return set;
+		}
+		else{
+			logger.warn("----geTuples--redisTemplate is null");
 		}
 		return null;
 	}
