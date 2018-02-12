fb73b721a68d878ee49eb9da6afc888586f0c3dd
joey <jianghao2>
2018-02-12 22:22:44 +0800

    update clustFlag

--- common-constant/src/main/java/com/babel/common/lottery/ClusterProcTime.java
+++ common-constant/src/main/java/com/babel/common/lottery/ClusterProcTime.java
@@ -7,4 +7,5 @@ public class ClusterProcTime {
     public static boolean HERMES_AGENT_RCOM = true;
 
     public static boolean HERMES_MEMBER_BAL = true;
+    public static boolean HERMES_USER_ORDER = true;
 }
