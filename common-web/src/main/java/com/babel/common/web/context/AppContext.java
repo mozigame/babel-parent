d74172f24dedab1f7c914b013de429db97ab1b96
Felix <felix_0515@163.com>
2018-04-16 10:06:14 +0800

    update ip

--- common-web/src/main/java/com/babel/common/web/context/AppContext.java
+++ common-web/src/main/java/com/babel/common/web/context/AppContext.java
@@ -116,19 +116,16 @@ public class AppContext {
 	}
 	
 	public static String getIpAddr(HttpServletRequest request) {
-		String ip = request.getHeader("x-real-ip");
-		if (CommUtil.isEmpty(ip)|| "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("x-forwarded-for");
-		}
-		if (CommUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
 
-		if (CommUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
 
-		if (CommUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
 		}
 		if("127.0.0.1".equals(ip) ||"0:0:0:0:0:0:0:1".equals(ip)){
