fd5607c32c1dfec2f26e3fb9f31a74e8bf9d8a19
clion <450145623@qq.com>
2018-04-19 13:57:43 +0800

    快十

--- common-constant/src/main/java/com/babel/common/lottery/LotteryIds.java
+++ common-constant/src/main/java/com/babel/common/lottery/LotteryIds.java
@@ -41,6 +41,8 @@ public enum LotteryIds {
     qq_ffc_1(31, "qq_ffc", LotteryType.SSC.code(), 1, "QQ分分彩"),
     qq_ffc_2(32, "qq_ffc", LotteryType.SSC.code(), 2, "QQ分分彩双面彩"),
     bj_q8_2(34, "bj_q8", LotteryType.Q8.code(), 2, "北京快8双面彩"),
+    gd_q10_2(36, "gd_q10", LotteryType.Q10_GD.code(), 2, "广东快10双面彩"),
+    gx_q10_2(38, "gx_q10", LotteryType.Q10_GX.code(), 2, "广西快10双面彩"),
     bc_ssc_1(101, "bc_ssc", LotteryType.SSC.code(), 1, "秒速时时彩"),
     bc_ssc_2(102, "bc_ssc", LotteryType.SSC.code(), 2, "秒速时时彩双面彩"),
     bc_11x5_1(103, "bc_11x5", LotteryType.ESF.code(), 1, "秒速11选5"),
