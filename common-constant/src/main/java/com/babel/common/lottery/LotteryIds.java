f7cebaea9aef3a98a1625390b0758fbd2ab3d9ac
Felix <felix_0515@163.com>
2018-05-01 15:57:07 +0800

    update 加拿大快乐8

--- common-constant/src/main/java/com/babel/common/lottery/LotteryIds.java
+++ common-constant/src/main/java/com/babel/common/lottery/LotteryIds.java
@@ -47,7 +47,7 @@ public enum LotteryIds {
     gd_q10_2(40, "gd_q10", LotteryType.Q10_GD.code(), 2, "广东快10双面彩"),
     tw_q8_1(41, "tw_q8", LotteryType.Q8.code(), 1, "台湾宾果"),
     tw_q8_2(42, "tw_q8", LotteryType.Q8.code(), 2, "台湾宾果双面彩"),
-    xb_q8_2(44,"xb_q8",LotteryType.Q8.code(),2,"加拿大西部快乐8双面彩"),
+    xb_q8_2(44,"xb_q8",LotteryType.Q8.code(),2,"加拿大快乐8双面彩"),
     lucky_q10_2(46,"lucky_q10",LotteryType.Q10_GD.code(),2,"幸运农场双面彩"),
     bc_ssc_1(101, "bc_ssc", LotteryType.SSC.code(), 1, "秒速时时彩"),
     bc_ssc_2(102, "bc_ssc", LotteryType.SSC.code(), 2, "秒速时时彩双面彩"),
